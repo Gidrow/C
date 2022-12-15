63791e6752d3cdb1a9f3e4eaa5aa05d82b04764b
Gidrow <FlightTU@gmail.com>
2022-12-15 23:15:34 +0300

    Dz1.1

--- DZ_1/DZ1.1/Program.cs
+++ DZ_1/DZ1.1/Program.cs
@@ -5,12 +5,11 @@ Console.Write("Введите первое число: ");
 int SN = int.Parse(Console.ReadLine()!);
 
 
-if(SN = FN*FN)
+if(FN == SN*SN)
 {
     Console.WriteLine("Первое число является квадратом второго");
-    Console.WriteLine("b");
 }
 else 
 {
-    Console.WriteLine("Первое число не квадрат второго");
+    Console.WriteLine("Первое число не является квадратом второго");
 }  
---
+++ DZ_1/DZ1.4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+
+int max;
+int min;
+
+min = 1;
+max = 1;
+
+if(b>a)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+
+if(max<c)
+{
+    max = c;
+}
+
+Console.Write("Максимальное число:  ");
+Console.Write(max);

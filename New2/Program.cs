63a4840fdf6630f0a7a68220559c38386b9ccfa5
Gidrow <FlightTU@gmail.com>
2022-12-15 21:49:10 +0300

    Задача #2

---
+++ New2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите первое число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if(a>b)
+{
+    Console.WriteLine("Наибольшее число: ");
+    Console.WriteLine("a");
+}
+else 
+{
+    Console.WriteLine("Наибольшее число: ");
+    Console.WriteLine(b);
+}  

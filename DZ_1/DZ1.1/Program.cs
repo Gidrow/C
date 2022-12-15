c6e8db37fe4d10f1eb926ff1cacdfba41e3a3486
Gidrow <FlightTU@gmail.com>
2022-12-15 22:25:56 +0300

    DZ1.1 START

---
+++ DZ_1/DZ1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int FN = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите первое число: ");
+int SN = int.Parse(Console.ReadLine()!);
+
+
+if(SN = FN*FN)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+    Console.WriteLine("b");
+}
+
+else 
+{
+    Console.WriteLine("Первое число не квадрат второго");
+}  

3dde67ad9a0d0dbc44dd7f1ff544f913466b6f9e
Gidrow <FlightTU@gmail.com>
2022-12-15 21:25:19 +0300

    Задание #1

--- New1/Program.cs
+++ New1/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+int result = a*a;
+Console.WriteLine("Корень числа");
+Console.WriteLine(result);

d96a921e77dc62399268d855899fb28f3df92055
Gidrow <FlightTU@gmail.com>
2022-12-15 23:11:10 +0300

    DZ1.2

--- DZ_1/DZ1.2/Program.cs
+++ DZ_1/DZ1.2/Program.cs
@@ -1,38 +1,53 @@
 ﻿Console.Write("Число: ");
 int a = int.Parse(Console.ReadLine()!);
-int a1 = 1;
-int a2 = 2;
-int a3 = 3;
-int a4 = 4;
-int a5 = 5;
-int a6 = 6;
-int a7 = 7;
 
-if(a = a1)
+int a1;
+int a2;
+int a3;
+int a4;
+int a5;
+int a6;
+int a7;
+
+a1 = 1;
+a2 = 2;
+a3 = 3;
+a4 = 4;
+a5 = 5;
+a6 = 6;
+a7 = 8;
+
+if(a == a1)
 {
     Console.WriteLine("Понедельник");
 }
-if(a = a2)
+
+if(a == a2)
 {
     Console.WriteLine("Вторник");
 }
-if(a = a3)
+
+if(a == a3)
 {
     Console.WriteLine("Среда");
 }
-if(a = a4)
+
+if(a == a4)
 {
     Console.WriteLine("Четверг");
 }
-if(a = a5)
+
+if(a == a5)
 {
     Console.WriteLine("Пятница");
 }
-if(a = a6)
+
+if(a == a6)
 {
     Console.WriteLine("Суббота");
 }
-if(a = a7)
+
+if(a == a7)
 {
     Console.WriteLine("Воскресенье");
 }

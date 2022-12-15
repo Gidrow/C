dab595e7aa2efa8ae3a1aa90965c8c0f4bdd79cc
Gidrow <FlightTU@gmail.com>
2022-12-15 23:01:03 +0300

    DZ3 COMPLETE

---
+++ DZ_1/DZ1.3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите первое число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите первое число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max;
+int min;
+
+min = 1;
+max = 1;
+
+if(b>a)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+
+if(max<c)
+{
+    max = c;
+}
+
+Console.Write("Максимальное число:  ");
+Console.Write(max);
+

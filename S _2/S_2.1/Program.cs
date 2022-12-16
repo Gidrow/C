2df12f41f6c0b08d1c6a1a997f4ea4848e156907
Gidrow <FlightTU@gmail.com>
2022-12-16 22:23:31 +0300

    S2

--- S _2/S_2.1/Program.cs
+++ S _2/S_2.1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Write("Введите первое число: ");
+// Double FN = Double.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите второе число: ");
+// Double SN = Double.Parse(Console.ReadLine()!);
+
+
+//  if ( FN % SN == 0 )
+//    {
+//        Console.Write("Первое число кратно второму");
+//    }
+//    else
+//    {
+//     Console.Write("Вывод остатка: ");
+//     Console.WriteLine(FN % SN);
+//   }
+
+
+// Console.WriteLine("Введите число: ");
+// var entNumber = Convert.ToInt32(Console.ReadLine());
+// var firstPer = 7;
+// var secondPer = 23;
+// if (entNumber % firstPer == 0 && entNumber % secondPer == 0)
+// {
+//     Console.Write("Введенное число кратно числам: ");
+//     Console.WriteLine(firstPer);
+//     Console.Write(" и ");
+//     Console.WriteLine(secondPer);
+// }
+// else
+// {
+//    Console.WriteLine("Введенное число не кратно заданным числам.");
+// }
+
+
+// Console.WriteLine("Введите число");
+// int N = int.Parse(Console.ReadLine()!);
+
+// int N1 = (N) * (-1);
+
+// while (N1 <= N)
+// {
+//     Console.WriteLine(N1);
+//     N1++;
+
+// }

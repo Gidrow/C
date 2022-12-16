0a1567296cd37785fa4aefbc4ec27a81a81c1cf1
Gidrow <FlightTU@gmail.com>
2022-12-16 22:39:52 +0300

    DZ_2.1

---
+++ DZ_2/DZ_2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a > 99 && a < 1000)
+{
+    int b = a / 100 * 10 + a % 10;
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Введино неверное число");
+
+}

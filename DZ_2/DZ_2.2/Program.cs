cf0fe216a67203dfe8a0fec796604004d9d30f66
Gidrow <FlightTU@gmail.com>
2022-12-17 00:51:36 +0300

    DZ_2

--- DZ_2/DZ_2.2/Program.cs
+++ DZ_2/DZ_2.2/Program.cs
@@ -1,26 +1,12 @@
-﻿// Забаное решение
-
-// Console.WriteLine("Введите трехзначное число ");
-// int a = int.Parse(Console.ReadLine()!);
-// int b;
-// int c;
-
-
-// b = (a / 100) * 100;
-
-// c = (a-b)/10;
-
-// Console.WriteLine(c);
-
-
-Console.WriteLine("Введите трехзначное число ");
+﻿Console.WriteLine("Введите число: ");
 int a = int.Parse(Console.ReadLine()!);
-int b = a / 100 % 10;
-if ( b % 10 > 0  )
+string b = a.ToString();
+
+if(a>100)
 {
-    Console.Write("Третье число ");
-    Console.WriteLine(b);
+Console.WriteLine(b[2]);
 }
-else {
-    Console.WriteLine("Третьего числа нет");
- }
+else
+{
+Console.WriteLine("Третьей цифры нет");
+}

91f4ed9a5a6d9f3a1e66088084d483e029315de9
Gidrow <FlightTU@gmail.com>
2022-12-17 01:14:24 +0300

    Fix DZ_2.1

--- DZ_2/DZ_2.1/Program.cs
+++ DZ_2/DZ_2.1/Program.cs
@@ -1,13 +1,10 @@
-﻿Console.WriteLine("Введите трехзначное число: ");
+﻿Console.WriteLine("Введите трехзначное число");
 int a = int.Parse(Console.ReadLine()!);
 
-if (a > 99 && a < 1000)
-{
-    int b = a / 100 * 10 + a % 10;
-    Console.WriteLine(b);
-}
-else
-{
-    Console.WriteLine("Введино неверное число");
+int b = a % 100;
 
-}
+int c = (b - (b % 10)) / 10; // Прим. a = 321     b = 21         c(2)= (21 - 1) / 10 
+
+Console.WriteLine("Второе число: " + c);
+
+

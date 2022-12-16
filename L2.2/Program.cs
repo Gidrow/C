a49c8afd00d65ea1a175b92ea26087769bff59f5
Gidrow <FlightTU@gmail.com>
2022-12-16 13:46:22 +0300

    Lesson 2

---
+++ L2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {1, 22, 33, 46, 53, 61, 78, 85, 9};
+array[0] = 12;
+Console.WriteLine(array[0]);
+

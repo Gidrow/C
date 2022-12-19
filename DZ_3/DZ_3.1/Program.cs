3791fc8e0943b90b269a9cf2f95feb2abc67404a
Gidrow <FlightTU@gmail.com>
2022-12-19 23:09:56 +0300

    DZ_3.1

---
+++ DZ_3/DZ_3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+String b = a.ToString();
+
+
+
+
+   if (b[0] == b[4] && b[1] == b[3])
+    {
+        Console.Write("Число является палиндромом. ");
+    }
+    else
+    {
+        Console.Write("Число не является палиндромом. ");
+    }

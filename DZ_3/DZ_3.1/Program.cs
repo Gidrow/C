772eb33f812dbffe0e31a721d4d79c20610659e3
Gidrow <FlightTU@gmail.com>
2022-12-20 00:04:34 +0300

    DZ3.1 Fix

--- DZ_3/DZ_3.1/Program.cs
+++ DZ_3/DZ_3.1/Program.cs
@@ -1,9 +1,27 @@
-﻿Console.WriteLine("Введите число: ");
+﻿// Console.WriteLine("Введите число: ");
+// int a = int.Parse(Console.ReadLine()!);
+// String b = a.ToString();
+
+
+
+
+//    if (b[0] == b[4] && b[1] == b[3])
+//     {
+//         Console.Write("Число является палиндромом. ");
+//     }
+//     else
+//     {
+//         Console.Write("Число не является палиндромом. ");
+//     }
+
+
+Console.WriteLine("Введите число: ");
 int a = int.Parse(Console.ReadLine()!);
 String b = a.ToString();
 
 
-
+ if (b.Length == 5)
+ {
 
    if (b[0] == b[4] && b[1] == b[3])
     {
@@ -13,3 +31,8 @@ String b = a.ToString();
     {
         Console.Write("Число не является палиндромом. ");
     }
+ }
+ else
+ {
+   Console.Write("Введено неверное число. "); 
+ }

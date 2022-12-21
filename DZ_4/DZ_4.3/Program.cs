950488b3ef9940fca4e6e5a3afd3bc8d2bf012ad
Gidrow <FlightTU@gmail.com>
2022-12-21 11:12:44 +0300

    DZ_4.3

---
+++ DZ_4/DZ_4.3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 случайных чисел (-10, 10) и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] array = new int[8];
+Random Random = new Random();
+
+Console.WriteLine();
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+
+    if (array[i] <= 8)
+    {
+    array[i] = Random.Next(-10, 10);
+    
+    Console.Write("  " + array[i] + "  ");
+    }
+
+}
+
+Console.WriteLine("]");
+Console.WriteLine();

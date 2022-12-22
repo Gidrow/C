e5530095f939b74833241e250543f051a96682b4
Gidrow <FlightTU@gmail.com>
2022-12-22 22:55:18 +0300

    DZ_5.3

---
+++ DZ_5/DZ_5.3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+
+int[] array = new int[10];
+Random Random = new Random();
+
+int Max = 1;
+int Min = 100;
+
+Console.WriteLine();
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+
+    array[i] = Random.Next(1, 101);
+    Console.Write("  " + array[i] + "  ");
+
+    if (array[i] > Max)
+    {
+        Max = array[i];
+    }
+
+    if (array[i] < Min)
+    {
+        Min = array[i];
+    }
+
+}
+Console.WriteLine("]");
+Console.WriteLine();
+Console.WriteLine($"Элемент массива с  минимальным  значением: {Min}");
+Console.WriteLine($"Элемент массива с максимальным  значением: {Max}");
+
+Console.WriteLine($"Разница между максимальным  {Max} и минимальным {Min} элементов массива.");
+Console.WriteLine();
+
+Console.WriteLine($"{Max} - {Min} = {Max -Min}"); 
+Console.WriteLine();
+

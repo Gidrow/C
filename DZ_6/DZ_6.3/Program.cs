f07937a0fa86e2e31b861a1674bae3d676c5e360
Gidrow <FlightTU@gmail.com>
2022-12-23 23:26:10 +0300

    Дополнительное задание

---
+++ DZ_6/DZ_6.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Доп. задача Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Пример:
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Введите десятичное число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+string D = Convert.ToString(N, 2);
+
+ Console.WriteLine();
+Console.WriteLine($"Десятичное число {N} в двоичной системе: {D}");
+ Console.WriteLine();

6bc0c1bcccedfc888dcf9926abcae9b8cefc4537
Gidrow <FlightTU@gmail.com>
2022-12-27 03:23:23 +0300

    Доп решение

--- DZ_7/DZ_7.3/Program.cs
+++ DZ_7/DZ_7.3/Program.cs
@@ -8,53 +8,17 @@
 
 
 
-// Console.Write("Введите количество строк: ");
-// int n = int.Parse(Console.ReadLine());
-
-// Console.Write("Введите количество столбцов: ");
-// int m = int.Parse(Console.ReadLine());
-
-// int sredArif = 0;
-
-// int[,] array = new int[n, m];
-
-
-// for (int i = 0; i < array.GetLength(0); i++)
-// {
-
-//     for (int j = 0; j < array.GetLength(1); j++)
-//     {
-//         array[i, j] = new Random().Next(0, 10);
-//         Console.Write(array[i, j] + " ");
-//         sredArif = sredArif + array[i, j] / m;
-//     }
-//     Console.WriteLine();
-//     Console.WriteLine(sredArif);
-// }
 
 
 
-double[,] array =
+int[,] array =
 {
     {1, 4, 7, 2},
     {5, 9, 2, 3},
     {8, 4, 2, 4}
 };
 
-
-double summ1 = 0;
-int Position1 = 0;
-
-double summ2 = 0;
-int Position2 = 1;
-
-double summ3 = 0;
-int Position3 = 2;
-
-double summ4 = 0;
-int Position4 = 3;
-
-
+int[] arraySumm = { 0, 0, 0, 0 };
 
 for (int i = 0; i < array.GetLength(0); i++)
 {
@@ -66,21 +30,98 @@ for (int i = 0; i < array.GetLength(0); i++)
     Console.WriteLine();
 }
 
+    int count = 0;
 
-for (int i = 0; i < array.GetLength(0); i++)
+for (int i = 0; i < 4; i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
+    double summ = 0;
+
+
+    double SummMeth()
     {
-        if (j == Position1) summ1 = summ1 + array[i, j];
-        if (j == Position2) summ2 = summ2 + array[i, j];
-        if (j == Position3) summ3 = summ3 + array[i, j];
-        if (j == Position4) summ4 = summ4 + array[i, j];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+
+                if (count == j) summ = summ + array[i, j];
+
+            }
+
+        }
+        return summ;
     }
-    Console.WriteLine();
+
+    double ys = SummMeth();
+    count++;
+    Console.Write($"            {Math.Round(ys/3, 2)}   ");
+    
+
 }
 
 
-Console.WriteLine($"{Math.Round(summ1/3, 2)} {Math.Round(summ2/3, 2)} {Math.Round(summ3/3, 2)} {Math.Round(summ4/3, 2)}"); 
 
-Console.WriteLine();
 
+
+
+
+
+
+
+
+
+// double[,] array =
+// {
+//     {1, 4, 7, 2},
+//     {5, 9, 2, 3},
+//     {8, 4, 2, 4}
+// };
+
+// int count = 0;
+
+// double summ = 0;
+
+// double summ1 = 0;
+// int Position1 = 0;
+
+// double summ2 = 0;
+// int Position2 = 1;
+
+// double summ3 = 0;
+// int Position3 = 2;
+
+// double summ4 = 0;
+// int Position4 = 3;
+
+
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         Console.Write(array[i, j] + " ");
+
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         if (j == Position1) summ1 = summ1 + array[i, j];
+//         if (j == Position2) summ2 = summ2 + array[i, j];
+//         if (j == Position3) summ3 = summ3 + array[i, j];
+//         if (j == Position4) summ4 = summ4 + array[i, j];
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Console.WriteLine($"{Math.Round(summ1/3, 2)} {Math.Round(summ2/3, 2)} {Math.Round(summ3/3, 2)} {Math.Round(summ4/3, 2)}"); 
+
+// Console.WriteLine();
